91fe071ea89ad4c6d2d985381ad8cf9bb458b99e
Veysel CANDABAKOGLU <veysel.77c@gmail.com>
2022-11-01 20:02:25 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: "blueviolet", color: "whitesmoke"}}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
